749a5b5d1085da868bb00d4b0c414c4624de0557
SND\oobenoob_cp <SND\oobenoob_cp@8ac85a5f-fc25-4b94-8d5e-fa21b7f00f96>
2011-06-29 21:58:05 +0000

    Provisional server core coding completed.  Ready to begin implementing Ares protocol.

---
+++ arca4/AresTCPPacketProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ares.IO;
+
+namespace arca4
+{
+    class AresTCPPacketProcessor
+    {
+        public static void Evaluate(UserObject userobj, ProtoMessage msg, AresTCPPacketReader packet)
+        {
+
+        }
+
+
+    }
+}
--- arca4/UserObject.cs
+++ arca4/UserObject.cs
@@ -29,6 +29,7 @@ namespace arca4
             this.ExternalIP = ((IPEndPoint)this.sock.RemoteEndPoint).Address;
             UserPool.SetID(this);
             this.LoggedIn = false;
+            this.Expired = false;
         }
 
         public void SocketTasks(uint now)
@@ -51,17 +52,41 @@ namespace arca4
             if (this.LoggedIn) // last update packet received?
             {
                 if ((this.timestamp + 240) < now)
-                    this.socket_health = 10;
+                    this.Expired = true;
             }
             else // loitering?
             {
                 if ((this.timestamp + 15) < now)
-                    this.socket_health = 10;
+                    this.Expired = true;
+            }
+
+            while (this.stack.Available)
+            {
+                try
+                {
+                    byte[] buf = this.stack.NextPacket;
+                    ProtoMessage msg = this.stack.Msg;
+
+                    if (!this.Expired)
+                        AresTCPPacketProcessor.Evaluate(this, msg, new AresTCPPacketReader(buf));
+                    else break;
+                }
+                catch { this.Expired = true; }
             }
         }
 
         public void Disconnect()
         {
+            while (this.data_out.Count > 0)
+            {
+                try
+                {
+                    this.sock.Send(this.data_out[0]);
+                    this.data_out.RemoveAt(0);
+                }
+                catch { break; }
+            }
+
             try { this.sock.Disconnect(false); }
             catch { }
             try { this.sock.Shutdown(SocketShutdown.Both); }
@@ -76,7 +101,7 @@ namespace arca4
         public bool Expired
         {
             get { return this.socket_health > 5; }
+            set { this.socket_health = value ? (uint)10 : (uint)0; }
         }
-
     }
 }
